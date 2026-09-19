f14fae953d9e65457bf83bd0c3c8a0595694c768
shuangyuema95-glitch <shuangyuema95@gmail.com>
2026-09-20 00:42:32 +0800

    Add files via upload

--- 01_metainfo_analysis.R
+++ 01_metainfo_analysis.R
@@ -28,14 +28,32 @@ p_country <- ggplot(country_df, aes(x = "", y = n, fill = country)) +
 print(p_country)
 
 ######2  sex prop pie
-sex_ration<-c(1,1,2,1,1,2,1,1,1,2,2,2,1,2,1,2,2,1,1,2,1,1,1,1,1,2,1,1,1,1,1,1,1)
-sex_df<-data.frame(sex=sex_ration)|>count(sex,name="n")|>mutate(group=case_when(sex==2~"Female",sex==1~"Male"),percent=n/sum(n)*100)
+#sex_ration<-c(1,1,2,1,1,2,1,1,1,2,2,2,1,2,1,2,2,1,1,2,1,1,1,1,1,2,1,1,1,1,1,1,1)
+sex_ration<-metainfo$sex
+sex_df<-data.frame(sex=sex_ration)|>count(sex,name="n")|>mutate(group=case_when(sex=="f"~"Female",sex=="m"~"Male"),percent=n/sum(n)*100)
 sex_col<-c("Male"="#6699CC","Female"="#86C7B4")
 p_sex<-ggplot(sex_df,aes(x="",y=n,fill=group))+geom_bar(stat="identity",width=1,color="white",linewidth=0.5)+coord_polar("y",start=0)+geom_text(aes(label=paste0(round(percent,1),"%")),position=position_stack(vjust=0.5),colour="black",size=4.2)+scale_fill_manual(values=sex_col)+theme_void()+theme(legend.title=element_blank(),legend.text=element_text(size=11,colour="black"),plot.title=element_text(hjust=0.5,size=14,colour="black"))
 print(p_sex)
 
+
+
+######sup  pathway prop pie
+pathway_col<-c(
+  "NFKB"="#B3D1E7",
+  "Inflammasome"="#0B71AB",
+  "Immune metabolism"="#A992C0",
+  "Interferon"="#C8BBD5",
+  "Cell death"="#5C6BC0",
+  "Uncategoried"="#EBB1A4")
+
+
+pathway_df <- metainfo %>% count(pathway, name = "n") %>% mutate(percent = n / sum(n) * 100) %>% arrange(desc(percent))
+pathway_df$pathway<-factor(pathway_df$pathway,levels = pathway_df$pathway)
+p_pathway<-ggplot(pathway_df,aes(x="",y=n,fill=pathway))+geom_bar(stat="identity",width=1,color="white",linewidth=0.5)+coord_polar("y",start=0)+geom_text(aes(label=paste0(round(percent,1),"%")),position=position_stack(vjust=0.5),colour="black",size=4.2)+scale_fill_manual(values=pathway_col)+theme_void()+theme(legend.title=element_blank(),legend.text=element_text(size=11,colour="black"),plot.title=element_text(hjust=0.5,size=14,colour="black"))
+
 library(patchwork)
-p_country|p_sex
+p_country|p_sex|p_pathway
+
 
 ######3 Bar plot for mutation type, novel or reported and genotype
 library(tidyverse)
@@ -284,3 +302,124 @@ age_density3plots<-distri_age_sex(
   med_lwd=0.7, line_w=0.8, fill_alpha=0.25, text_size=2.8)
 age_density3plots
 
+###### gene count for all mutations
+###### Function
+library(openxlsx); library(ggplot2); library(dplyr); library(patchwork)
+genemeta <- read.xlsx("C:/Users/Administrator/Desktop/15个基因已发表突变中是JIA 表型的搜集/score_meta.xlsx")
+
+plot_gene_bar <- function(genemeta,
+                          inher_colors = c("autosomal dominant"="#FEC260",
+                                           "Autosomal recessive"="#6699CC",
+                                           "X-linked recessive"="#AF478A")){
+  library(ggplot2); library(dplyr)
+  genemeta <- genemeta[!genemeta$Domain %in% c("\\","NA","") & !is.na(genemeta$Domain), ]
+  gene_n <- genemeta %>% count(Gene, name = "n")
+  inherit_df <- genemeta %>% group_by(Gene) %>%
+    summarise(Inheriance = unique(Inheriance)[1], .groups = "drop") %>%
+    left_join(gene_n, by = "Gene")
+  inherit_df$Inheriance <- factor(inherit_df$Inheriance,
+                                  levels = c("autosomal dominant","Autosomal recessive","X-linked recessive"))
+  inherit_df <- inherit_df %>% arrange(Inheriance, desc(n))
+  inherit_df$Gene <- factor(inherit_df$Gene, levels = inherit_df$Gene)
+  inherit_df$pct <- inherit_df$n / sum(inherit_df$n)
+  inherit_df$label <- paste0(round(inherit_df$pct*100,1), "%")
+  p <- ggplot(inherit_df, aes(x = Gene, y = pct)) +
+    geom_col(aes(fill = Inheriance), width = 0.48, color = NA) +
+    geom_text(aes(label = label), vjust = -0.5, size = 3, color = "black") +
+    scale_y_continuous(labels = scales::percent, expand = expansion(mult = c(0, 0.08))) +
+    scale_fill_manual(values = inher_colors) +
+    facet_grid(~ Inheriance, scales = "free_x", space = "free_x") +
+    labs(x = "", y = "Percentage") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, color = "black"),
+          axis.text.y = element_text(color = "black"),
+          strip.text = element_text(face = "bold"),
+          panel.grid = element_blank(),
+          legend.position = "none")
+  return(p)
+}
+p <- plot_gene_bar(genemeta)
+p
+
+######6 gene domain for all mutations. Donut plots
+library(openxlsx); library(ggplot2); library(dplyr); library(patchwork)
+genemeta <- read.xlsx("C:/Users/Administrator/Desktop/15个基因已发表突变中是JIA 表型的搜集/score_meta.xlsx")
+genemeta <- genemeta[!genemeta$Domain %in% c("\\","NA","") & !is.na(genemeta$Domain), ]
+genemeta$Domain_short <- gsub(" domain", "", genemeta$Domain, ignore.case = TRUE)
+gene_n <- genemeta %>% count(Gene, name = "n")
+inherit_df <- genemeta %>% group_by(Gene) %>%
+  summarise(Inheriance = unique(Inheriance)[1], .groups = "drop") %>%
+  left_join(gene_n, by = "Gene")
+inherit_df$Inheriance <- factor(inherit_df$Inheriance,
+                                levels = c("autosomal dominant","Autosomal recessive","X-linked recessive"))
+gene_order <- inherit_df %>% arrange(Inheriance, desc(n)) %>% pull(Gene)
+inher_colors <- c("Autosomal recessive" = "#6699CC",
+                  "autosomal dominant" = "#FEC260",
+                  "X-linked recessive" = "#AF478A")
+domain_df <- genemeta %>%
+  count(Gene, Domain_short, name = "n") %>%
+  group_by(Gene) %>%
+  arrange(desc(n), .by_group = TRUE) %>%
+  mutate(prop = n / sum(n),
+         label = paste0(Domain_short, "\n", round(prop*100,1), "%")) %>%
+  ungroup()
+domain_palette <- c("#6699CC","#007ABA","#9CD2ED","#86C7B4","#847AB3","#F1BAAB")
+plot_list <- list()
+for(g in gene_order){
+  sub <- domain_df %>% filter(Gene == g) %>% arrange(desc(n))
+  n_dom <- nrow(sub)
+  sub_colors <- setNames(domain_palette[1:n_dom], sub$Domain_short)
+  inh <- inherit_df$Inheriance[inherit_df$Gene == g]
+  title_color <- inher_colors[inh]
+  if(is.na(title_color)) title_color <- "black"
+  p <- ggplot(sub, aes(x = 2, y = prop, fill = Domain_short)) +
+    geom_col(width = 1, color = NA) +
+    coord_polar(theta = "y") +
+    xlim(0.5, 2.5) +
+    geom_text(aes(label = label), position = position_stack(vjust = 0.5),
+              size = 2.5, color = "black") +
+    scale_fill_manual(values = sub_colors) +
+    ggtitle(g) +
+    theme_void() +
+    theme(plot.title = element_text(hjust = 0.5, size = 10, face = "bold", color = title_color),
+          legend.position = "none")
+  plot_list[[g]] <- p
+}
+p <- wrap_plots(plot_list, nrow = 3, ncol = 5)
+p
+
+######7 CADD and frequency
+library(openxlsx); library(ggplot2); library(dplyr); library(patchwork)
+genemeta <- read.xlsx("C:/Users/Administrator/Desktop/15个基因已发表突变中是JIA 表型的搜集/score_meta.xlsx")
+data1<-genemeta[which(!is.na(genemeta$CADD)),]%>%filter(!is.na(GnomadV4))
+plot_cadd_gnomad <- function(data, x_var = "CADD", point_size = 3, point_stroke = 0,
+                             gene_order = c("TNFRSF1A","NLRP3","COPA","TNFAIP3","PSTPIP1","ADA2","LACC1","MVK","LPIN2","ELF4"),
+                             title = "Summary of genes associated with arthritis"){
+  library(ggplot2); library(dplyr)
+  data$Gene <- factor(data$Gene, levels = gene_order)
+  gene_colors <- c("TNFRSF1A"="#FEC260","NLRP3"="#FEC260","COPA"="#FEC260","TNFAIP3"="#FEC260","PSTPIP1"="#FEC260",
+                   "ADA2"="#6699CC","LACC1"="#6699CC","MVK"="#6699CC","LPIN2"="#6699CC",
+                   "ELF4"="#AF478A")
+  if(x_var == "CADD"){
+    p <- ggplot(data, aes(x = CADD, y = GnomadV4, fill = Gene)) +
+      geom_point(size = point_size, shape = 21, stroke = point_stroke, color = "black") +
+      geom_vline(xintercept = 20, linetype = "dashed", color = "grey50") +
+      scale_y_log10() + scale_fill_manual(values = gene_colors, guide = "none") +
+      labs(x = "CADD score", y = "gnomAD AF (log10)")
+  } else {
+    p <- ggplot(data, aes(x = GnomadV4, y = CADD, fill = Gene)) +
+      geom_point(size = point_size, shape = 21, stroke = point_stroke, color = "black") +
+      geom_hline(yintercept = 20, linetype = "dashed", color = "grey50") +
+      scale_x_log10() + scale_fill_manual(values = gene_colors, guide = "none") +
+      labs(x = "gnomAD AF", y = "CADD score")
+  }
+  p + facet_wrap(~ Gene, nrow = 2, scales = "free") +
+    ggtitle(title) + theme_bw() +
+    theme(axis.text = element_text(color = "black"),
+          plot.title = element_text(hjust = 0.5, size = 12, face = "bold"),
+          strip.text = element_text(face = "bold", size = 9),
+          panel.grid = element_blank(), legend.position = "none")
+}
+p1 <- plot_cadd_gnomad(data1, x_var = "CADD", point_size = 2.6,point_stroke = 0.5)
+#p2 <- plot_cadd_gnomad(data1, x_var = "AF", point_stroke = 0.5)
+p1
